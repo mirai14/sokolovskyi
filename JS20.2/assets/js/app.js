e05e736040d4d3aff14703c8d690dd04bae03f25
mirai14 <sergiyfalcone@gmail.com>
2021-11-17 16:01:39 +0200

    Update app.js

--- JS20.2/assets/js/app.js
+++ JS20.2/assets/js/app.js
@@ -1,6 +1,20 @@
 
     // 3-1000 //
 
+    for (let i = 3; i <= 1000; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                break;
+            }
+            if (j == i - 1) {
+                console.log(i);
+                break;
+            }
+        }
+    } console.log(1000);
+   
+    console.log(``); console.log(``); console.log(``);
+    
     n1k:
     for (let i = 3; i <= 1000; i++) {
         for (let j = 2; j < i; j++) {
@@ -8,5 +22,7 @@
         }
         console.log(i);
     }
+
+
 
         
