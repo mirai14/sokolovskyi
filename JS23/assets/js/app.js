5dbd939ed6693e5bb2d85dc9ebeeb77e3e76554b
mirai14 <sergiyfalcone@gmail.com>
2021-12-01 22:44:07 +0200

    up

--- JS23/assets/js/app.js
+++ JS23/assets/js/app.js
@@ -4,23 +4,37 @@
     secret = Math.floor(secret);
     secret = secret + 1;
 
+    let i = 10;
+
     function game(){
-        for(let i = 1; i <= 10; i++){
+        
 
+            if(i < 1){
+                return;
+            }
+        
             let option = +numberInput.value;
-
-            attempt.innerHTML = (`${10-i}`);
 
             if(secret == option){
                 main.innerHTML = (`${option}`);
-                number.innerHTML = (`Ваш варіант ${option} - вірний!`);
-                break;
+                number.innerHTML = (`Ваш варіант ${option} - вірний. Гра закінчена!`);
+                i=0;
+                return;
             } else if(secret > option){
                 number.innerHTML = (`Ваш варіант ${option} - менший за моє число`);
             } else {
                 number.innerHTML = (`Ваш варіант ${option} - більший за моє число`);
             }
-    
-        }
+
+            i--;
+
+            attempt.innerHTML = (`${i}`);
+
+            if(i < 1){
+                main.innerHTML = (`${secret}`);
+                number.innerHTML = 'Ви не вгадали. Гра закінчена!';
+
+            }
 
     }
+
